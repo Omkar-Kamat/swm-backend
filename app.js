b9805e77450aac84a4a2179c92a9e161c94e2cd9
Omkar-Kamat <mail4omkarkamat@gmail.com>
2024-03-27 14:53:12 +0530

    swm backend

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const app = express();
 const PORT = process.env.PORT;
 const survey = require('./server/routes/survey');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 
 // middleware
 app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:3000","https://pro-che-swm.onrender.com"]
+}));
 
 app.use((req,res,next)=>{
     next();
